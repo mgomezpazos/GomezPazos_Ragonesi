607d1eda15d8dc1c53cc1b9083a265137a56b26e
Fragoesi <fragonesi@udesa.edu.ar>
2023-03-16 22:53:27 -0300

    arreglo de la función insertar

--- Tipos.hs
+++ Tipos.hs
@@ -7,7 +7,7 @@ type Nombre = String
 insertar :: Ord a => a -> [a] -> [a] 
 --que entrega una nueva lista con los elementos originales y el agregado ordenados naturalmente.
 --insertar x xs  = [flip(x : xs)]
-insertar x xs = reverse(x:reverse(xs))
+insertar elemento lista = reverse(elemento:reverse(lista))
 
 --(+++) xs = foldr pegarAtras xs . reverse
 
