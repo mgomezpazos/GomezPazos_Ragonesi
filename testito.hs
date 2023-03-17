1da279892f36918f558d3d05ca7b62fcbd8ea4b4
Manuela Gomez Pazos <mgomezpazos@udesa.edu.ar>
2023-03-17 16:00:08 -0300

    Subiendo nuestro archivo para hacer testing

---
+++ testito.hs
@@ -0,0 +1,16 @@
+module Tema (Tema, nuevoT)
+    where
+
+import Tipos
+data Tema = Tem Nombre [Etiqueta] Datos deriving (Eq,Show, Ord)
+--El tipo Datos representa la secuencia de datos que conforma el archivo mp3
+{-
+ 
+nombres, [etiqueta], datos son strings, hay que pensarlos como un ALIAS. No son mas que strings
+Tem es el constructor, me va a construir un tipo de dato Tema.
+Para construir un valor con Tem, tengo que pasarle 3 elementos. Nombre [Etiqueta] Datos
+Como la función no recibe Etiqueta, le puedo pasar listas con cero elementos
+tema_1 = Tem(Nombre [Etiqueta] Datos)
+-}
+nuevoT :: Nombre -> Datos -> Tema 
+nuevoT nombre datos = Tem nombre [] datos
