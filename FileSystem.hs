6cf82f571fe9ab1917256d53a50e0218aa7237c2
Manuela Gomez Pazos <mgomezpazos@udesa.edu.ar>
2023-03-22 14:51:08 -0300

    Actualizaciones

--- FileSystem.hs
+++ FileSystem.hs
@@ -1,18 +1,18 @@
-module FileSystem ( FileSystem, nuevoF, etiquetasF, temasF, agregarF, filtrarF )
+module FileSystem ( FileSystem, {-nuevoF, etiquetasF, temasF, agregarF, filtrarF-} )
     where
 import Tipos
 import Tema
 data FileSystem = FS [Etiqueta] [Tema] deriving (Eq, Show)
 
-nuevoF :: FileSystem
+--nuevoF :: FileSystem
 --Crea un nuevo FileSystem con sus listas vacias. (Crea una lista de listas vacías)
-etiquetasF :: FileSystem → [ Etiqueta ]
+--etiquetasF :: FileSystem -> [ Etiqueta ]
 --Crea la lista de etiquetas
-temasF :: FileSystem → [ Tema ]
+--temasF :: FileSystem -> [ Tema ]
 --Crea la listas de temas
-agregarF :: Tema →FileSystem →FileSystem
+--agregarF :: Tema -> FileSystem -> FileSystem
 --Agrega el tema y sus etiquetas de ser necesario.
-filtrarF :: Etiqueta → FileSystem → [ Tema ]
+--filtrarF :: Etiqueta -> FileSystem -> [ Tema ]
 --Le pasamos una etiqueta y nos dice la lista de temas con esa etiqueta. Usa las funciones de el file Tema.
 
 {-
