abc2f646ce0ad2d5a37e482b2bf3967fde59fd8f
Manuela Gomez Pazos <mgomezpazos@udesa.edu.ar>
2023-03-25 10:49:14 -0300

    Tests de filesystem LISTOS!!!

--- Playlist.hs
+++ Playlist.hs
@@ -26,6 +26,22 @@ resetP (Play songnum temas) = Play 0 temas --tested!!
 
 -- TEST:
 
+cancionTest1 :: Tema
+cancionTest1 = nuevoT "Borderline" "Tame_Impala_music"
+cancionTest2 :: Tema
+cancionTest2 = nuevoT "Despacito" "Des-pa-cito"
+cancionTest3 :: Tema
+cancionTest3 = nuevoT "Eternal Summer" "The_Strokes_Song"
+
+test_tema :: [Bool]
+test_tema = [
+    nuevaP [cancionTest1, cancionTest2, cancionTest3] == Play 0 [cancionTest1, cancionTest2, cancionTest3],
+    actualP (Play 2 [cancionTest1, cancionTest2, cancionTest3]) == cancionTest3,
+    skipP (Play 1 [cancionTest1, cancionTest2, cancionTest3]) == Play 2 [cancionTest1, cancionTest2, cancionTest3],
+    backP (Play 1 [cancionTest1, cancionTest2, cancionTest3]) == Play 0 [cancionTest1, cancionTest2, cancionTest3],
+    resetP (Play 1 [cancionTest1, cancionTest2, cancionTest3]) == Play 0 [cancionTest1, cancionTest2, cancionTest3]
+    ] 
+
 {-}
 test_tema :: [Bool]
 test_tema = [
