4bfde32fb0a1cc1c3fadf79944e665674eaf948d
Manuela Gomez Pazos <mgomezpazos@udesa.edu.ar>
2023-04-05 17:32:41 -0300

    updates

--- TP_1/Tema.hs
+++ TP_1/Tema.hs
@@ -23,13 +23,13 @@ aplicaT :: Etiqueta -> Tema -> Bool --nos dice si esa etiqueta ya pertenece al t
 aplicaT etiqueta (Tem _ etiquetas _) = etiqueta `elem` etiquetas
 
 
--- TEST:
+-- -- TEST:
 test_tema :: [Bool]
 test_tema = [
     nuevoT "a" "b" == Tem "a" [] "b",
-    nombreT (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == "Borderline",
-    datosT (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == "Tame_Impala_Song",
-    etiquetasT (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == ["indie", "alternative", "Tame Impala"] ,
-    agregarT "pop" (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == Tem "Borderline" ["indie", "alternative", "Tame Impala", "pop"] "Tame_Impala_Song",
-    aplicaT "indie" (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song")
-    ]
+     nombreT (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == "Borderline",
+     datosT (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == "Tame_Impala_Song",
+     etiquetasT (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == ["indie", "alternative", "Tame Impala"] ,
+     agregarT "pop" (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song") == Tem "Borderline" ["indie", "alternative", "Tame Impala", "pop"] "Tame_Impala_Song",
+     aplicaT "indie" (Tem "Borderline" ["indie", "alternative", "Tame Impala"] "Tame_Impala_Song")
+ ]
